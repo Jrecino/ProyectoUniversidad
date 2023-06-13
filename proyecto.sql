6f39b3ada8e74fac9e79f5cceb61c41770b60c60
Jrecinos <jrecinosm4@miumg.edu.gt>
2023-06-13 09:27:11 -0600

    ver-1.1

---
+++ proyecto.sql
@@ -0,0 +1,7 @@
+use bd_mascotas
+INSERT INTO mascotas (tipo_animal, edad, nombre, diagnostico, procedimiento)
+VALUES ('Perro', 3, 'Max', 'Fiebre', 'Receta: Descanso y medicamento X');
+select * from mascotas;
+ 
+
+
